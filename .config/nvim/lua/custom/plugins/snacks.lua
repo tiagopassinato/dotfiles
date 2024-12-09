2dc53d4d4e78c2584f95d7bfc90696393262431a
Tiago Passinato <tiagopassinato@gmail.com>
2024-12-09 10:31:19 -0500

    snacks vim

---
+++ .config/nvim/lua/custom/plugins/snacks.lua
@@ -0,0 +1,30 @@
+return {
+  'folke/snacks.nvim',
+  priority = 1000,
+  lazy = false,
+  opts = {
+    dashboard = { enabled = true },
+    git = { enabled = true },
+    lazygit = { enabled = true },
+    notifier = { enabled = true },
+    quickfile = { enabled = true },
+    statuscolumn = { enabled = true },
+    words = { enabled = true },
+  },
+  keys = {
+    {
+      '<leader>gg',
+      function()
+        Snacks.lazygit()
+      end,
+      desc = 'Lazygit',
+    },
+    {
+      '<leader>y',
+      function()
+        Snacks.terminal()
+      end,
+      desc = 'Toggle Terminal',
+    },
+  },
+}
